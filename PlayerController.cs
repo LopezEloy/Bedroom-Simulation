7051b7e08d8cd5cb966c6e2bd99210086b8346c9
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2024-09-24 01:22:28 -0500

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -65,10 +65,12 @@ public class PlayerController : MonoBehaviour
 
     void FaceTarget()
     {
+        if (agent.velocity != Vector3.zero)
         {
             Vector3 direction = (agent.destination - transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+        }
     }
 
     void SetAnimations()
